d19cb2081469362a76ed1127113775c1d75e85d4
Tom Fricker <fricker_tom@hotmail.com>
2022-08-24 22:00:11 +0100

    Add new id when adding to Dg

--- src/ToDo.List.Api/Repositories/ToDoRepository.cs
+++ src/ToDo.List.Api/Repositories/ToDoRepository.cs
@@ -19,6 +19,8 @@ namespace ToDo.List.Api.Repositories
 
         public void AddToDo(Data.Models.ToDo toDo)
         {
+            toDo.Id = Guid.NewGuid();
+
             toDoDb.AddToDo(toDo);
         }
 
--- test/ToDo.List.Api.Tests.Unit/ToDoRepositoryShould.cs
+++ test/ToDo.List.Api.Tests.Unit/ToDoRepositoryShould.cs
@@ -35,5 +35,18 @@ namespace ToDo.List.Api.Tests.Unit
             Assert.Equal(5, toDoListResult.Count());
             Assert.Contains(toDo, toDoListResult);
         }
+
+        [Fact]
+        public void AddNewGuidId_WhenAddToDoCalled()
+        {
+            var toDo = new Data.Models.ToDo();
+
+            toDoRepository.AddToDo(toDo);
+
+            var toDoListResult = toDoTestFixture.GetToDos();
+            var toDoIds = toDoListResult.Select(t => t.Id);
+
+            Assert.DoesNotContain(Guid.Empty, toDoIds);
+        }
     }
 }
